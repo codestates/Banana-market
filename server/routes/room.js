314ae4435b90d9708ad1e0a9c0eadcb1f927b76c
ParkShinyeong <89335240+ParkShinyeong@users.noreply.github.com>
2022-02-15 10:25:54 +0900

    Revert "[Server] #3 add: database model"

--- server/routes/room.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const controller = require("./../controller");
-
-router.get("/messages/:postid", controller.getMessage);
-router.get("/", controller.getRoomList);
-router.post("/join/:postid", controller.joinRoom);
-router.get("/participant/:postid", controller.getParticipant);
-
-module.exports = router;

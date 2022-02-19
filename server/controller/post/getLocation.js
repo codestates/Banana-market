9884eefc7601b6da82c7208393a4e7f81475d13f
Park shinyeong <syngh503@gmail.com>
2022-02-20 00:55:22 +0900

    Feat: Get user's location

--- server/controller/post/getLocation.js
+++ server/controller/post/getLocation.js
@@ -1,3 +1,36 @@
+const { User, Region } = require("../../models");
+const { checkAccessToken } = require("../tokenFunction");
+
 module.exports = async (req, res) => {
-  res.status(200).send("");
+  // 사용자 지역 정보 요청
+  const accessTokenData = checkAccessToken(req);
+  if (!accessTokenData) {
+    return res.status(401).send("Not authorized");
+  }
+
+  const { id } = accessTokenData;
+
+  const userData = await User.findOne({
+    where: { id },
+  });
+
+  const { region_id } = userData.dataValues;
+
+  Region.findOne({
+    where: {
+      id: region_id,
+    },
+  })
+    .then((regionData) => {
+      res.status(200).send({
+        data: {
+          region: regionData.dataValues.city,
+        },
+        message: "ok",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send({ message: "Internal server error" });
+    });
 };

b22ce280940a4867cb2e4ab834af7dd03b7504d0
Park shinyeong <syngh503@gmail.com>
2022-02-24 16:55:31 +0900

    Feat: Google Social login 구글 소셜 로그인 기능 구현 소셜 로그인 시 user type은 'SOCIALUSER' 패스워드는 null로 지정

--- server/controller/auth/googleAccessToken.js
+++ server/controller/auth/googleAccessToken.js
@@ -1,30 +1,82 @@
-// const { OAuth2Client } = require('google-auth-library');
-// require('dotenv').config();
-// const token = require('./token');
-// const client = new OAuth2Client(process.env.CLIENT_ID);
-// module.exports = async (req, res) => {
-//   function verify() {
-//     const ticket = await client.verifyIdToken({
-//       idToken: token,
-//       audience: process.env.CLIENT_ID,
-//     });
-//     const payload = ticket.getPayload();
-//     const userid = payload['sub'];
-//   }
-//   verify().catch(console.error);
-// };
 const axios = require('axios');
+const { User } = require('../../models');
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  sendAccessToken,
+  sendRefreshToken,
+} = require('../tokenFunction');
+
 module.exports = async (req, res) => {
   const code = req.body.authorizationCode;
-  const url = `https://oauth2.googleapis.com/token?code=${code}&client_id=${process.env.GOOGLE_CLIENT_ID}&client_secret=${process.env.GOOGLE_CLIENT_SECRET}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&grant_type=${process.env.GOOGLE_GRANT_TYPE}`;
-  const access_token = await axios
-    .post(url, {
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    })
-    .then((el) => {
-      return el.data.access_token;
+  const url = `https://oauth2.googleapis.com/token?code=${code}&client_id=${process.env.GOOGLE_CLIENT_ID}&client_secret=${process.env.GOOGLE_CLIENT_SECRET}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&grant_type=authorization_code`;
+  const getAccessToken = await axios
+    .post(
+      url,
+      {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        code,
+      },
+      {
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Internal server error');
+    });
+
+  if (!getAccessToken) {
+    return res.status(401).send('Invalid accessToken');
+  }
+
+  const accessToken = getAccessToken.data.access_token;
+
+  const googleAPI = `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${accessToken}`;
+  const userData = await axios
+    .get(googleAPI, {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
     })
     .catch((err) => {
-      console.log('err=', err);
+      console.log(err);
+      res.status(500).send('Internal server error');
+    });
+  const { email, verified_email, name, picture } = userData.data;
+  // console.log(userinfo);
+
+  if (!verified_email) {
+    return res.status(404).send({ message: 'block user' });
+  }
+
+  const randomNum = Math.floor(Math.random() * 100000);
+  await User.findOrCreate({
+    where: { email },
+    defaults: {
+      email,
+      name: `빠나나 용사 ${randomNum}`,
+      profile_image_location: picture,
+      region_id: 1,
+      type: 'SOCIALUSER',
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        return res.status(404).send('Not found user');
+      }
+      const userInfo = data[0].dataValues;
+      console.log(userInfo);
+      delete userInfo.password;
+      const localAccessToken = generateAccessToken(userInfo);
+      const localRefreshToken = generateRefreshToken(userInfo);
+      sendAccessToken(res, localAccessToken);
+      sendRefreshToken(res, localRefreshToken);
+      res.status(200).send({ message: 'ok' });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send({ message: 'Internal server error' });
     });
 };
--- server/controller/tokenFunction/index.js
+++ server/controller/tokenFunction/index.js
@@ -1,35 +1,35 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   // access token 생성
   generateAccessToken: (data) => {
-    return jwt.sign(data, process.env.ACCESS_SECRET, { expiresIn: "1d" });
+    return jwt.sign(data, process.env.ACCESS_SECRET, { expiresIn: '1d' });
   },
   // refresh token 생성
   generateRefreshToken: (data) => {
-    return jwt.sign(data, process.env.REFRESH_SECRET, { expiresIn: "30d" });
+    return jwt.sign(data, process.env.REFRESH_SECRET, { expiresIn: '30d' });
   },
   // 쿠키로 access token 전달
   sendAccessToken: (res, accessToken) => {
-    res.cookie("accessToken", accessToken, {
+    res.cookie('accessToken', accessToken, {
       httpOnly: true,
       // signed: true,
-      //   domain: process.env.SERVER_DOMAIN,
-      //   path: "/",
-      //   secure: true,
-      //   sameSite: "none",
+      // domain: process.env.SERVER_DOMAIN,
+      // path: '/',
+      // secure: true,
+      // sameSite: 'none',
     });
   },
   // 쿠키로 refresh token 전달
   sendRefreshToken: (res, refreshToken) => {
-    res.cookie("refreshToken", refreshToken, {
+    res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       // signed: true,
-      //   domain: process.env.SERVER_DOMAIN,
-      //   path: "/",
-      //   secure: true,
-      //   sameSite: "none",
+      // domain: process.env.SERVER_DOMAIN,
+      // path: '/',
+      // secure: true,
+      // sameSite: 'none',
     });
   },
   // access token 유효성 확인

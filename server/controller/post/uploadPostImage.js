f171d7b4d8d65b1215cab765ccde1c241e2b6af6
Park shinyeong <syngh503@gmail.com>
2022-02-25 18:33:29 +0900

    Feat: Upload article image

---
+++ server/controller/post/uploadPostImage.js
@@ -0,0 +1,28 @@
+const { Articles } = require('../../models');
+const uploadImage = require('../../modules/multerUploadImg');
+const articleImage = uploadImage.single('image');
+
+module.exports = async (req, res) => {
+  //   console.log(req.file);
+  //   res.status(200).send('ok');
+  let location = null;
+  let key = null;
+
+  articleImage(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ message: 'Fail image upload' });
+    }
+
+    location = req.file.transforms[0].location;
+    key = req.file.transforms[0].key;
+
+    res.status(201).send({
+      data: {
+        imageLocation: location,
+        imageKey: key,
+      },
+      message: 'ok',
+    });
+  });
+};

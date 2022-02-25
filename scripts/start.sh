128ff75a9f07add9c28812f39d1ea961d971aed4
DAEUNY <desmap@gmail.com>
2022-02-25 18:36:44 +0900

    Chore: Modify start.sh & index.js
    - 소셜로그인 환경변수 추가
    - 서버 포트 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,8 +8,14 @@ export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 
 
-# export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
-# export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
+
+export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_SECRET --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_REDIRECT_URI=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
+
+
 
 sleep 10s && pm2 status // pm2동작을 확인 - 제대로 작동했는지, codedeploy ->로그 확인
 echo $DATABASE_HOST     // 환경변수에 DATABASE_HOST의 값이 무엇인지 확인

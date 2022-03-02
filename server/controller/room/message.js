79ff4ecfc7c94e1cd1785ae961c014829f1c5611
DAEUNY <desmap@gmail.com>
2022-03-02 13:46:17 +0900

    Feat: Modify all room features
    - join.js
    -> 참여한 채팅방인지 검사
    -> current/total 인원 검사
    -> userArticles 테이블에 참여 후 current/total 및 status 변경
    
    - message.js
    -> 유저가 참여한 시간보다 이후의 채팅메세지만 받기 (userArticles 레코드 검사)
    
    - participant.js
    -> 유저의 id attribute 추가 (실시간 채팅시 메세지 주고받는 이벤트에 사용 예정)
    
    - room.js
    -> raw:true 옵션으로 모든 정보가 단일 객체로 반환되는 것 수정 (article-chat: nested하게 포함되야 함)

--- server/controller/room/message.js
+++ server/controller/room/message.js
@@ -1,4 +1,4 @@
-const { User, Article, Chat } = require('../../models');
+const { User, Article, Chat, UserArticles } = require('../../models');
 const { Op } = require("sequelize");
 const { checkAccessToken } = require('../tokenFunction');
 
@@ -9,12 +9,27 @@ module.exports = async (req, res) => {
     return res.status(401).send({ message: "Not authorized" });
   }
 
-  const { articleid } = req.params
+  // 1. 아이디 받아서 UserArticles에서 레코드 생긴 시간 확인
+  // 2. Chat에 조건으로 1번 시간보다 큰 것만 
 
-  // 
+  const { articleid } = req.params
+  const { id: userId } = accessTokenData
+
+  // articleid 없이 요청 보냈을 때
   if (!articleid) {
     return res.status(400).send({ message: 'Article id required' });
   }
+
+  // 유저가 참여한 시간 확인
+  let userJoinAt = await UserArticles.findOne({
+    where: {
+      user_id: userId,
+      article_id: articleid
+    },
+    attributes: ['createdAt']
+  }).catch((err) => {
+    res.status(500).send({message : 'Internal server error'})
+  })
 
   // article 별 message list
   const articleMessages = await Article.findAll({
@@ -28,16 +43,21 @@ module.exports = async (req, res) => {
     include : [{
       model : Chat,
       attributes : ['contents', 'createdAt'],
+      where : {
+        createdAt : {
+          [Op.gte]: userJoinAt
+        }
+      },
       include : [{
         model : User,
-        attributes : ['name', ['profile_image_location', 'profileImage']]
+        attributes : ['id', 'name', ['profile_image_location', 'profileImage']]
       }]
     }]
   }).catch((err) => {
     res.status(500).send({message : 'Internal server error'})
   })
-  
 
+  // client에 보낼 데이터 articleMessages flat 버전
   let userChat = {}
   await articleMessages.map((article) => {
     article = article.toJSON()
@@ -62,7 +82,11 @@ module.exports = async (req, res) => {
       messageList : chats
     }
     return userChat
+  }).catch((err) => {
+    res.status(500).send({message : 'Internal server error'})
   })
+
+  // console.log("   --- - - - -- - -  flat 변경", userChat)
 
   const data = { ...userChat }
 
